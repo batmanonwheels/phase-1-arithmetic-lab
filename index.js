ec97f167dc8a97e04aab404d1a8e06882c6c1a15
Devin <devingarcia5423@gmail.com>
2022-05-16 16:35:35 -0400

    commit tree fc66d9c9050cfd53cc03314275a006323d7b40f0

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let multiply = function() {
+    const num1 = 31;
+    const num2 = 2;
+    return num1 * num2;
+}
 
-// Write your code here
+
+// let random = Math.random() + 1
+
+// let mod =
+
+// let max = Math.max(1, 2, 3, 4, 5, 20)
